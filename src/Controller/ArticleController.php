8e34edde4895ebe0a2d8325f0930833a61ab36e4
helene-cezard <helene.cezard@gmail.com>
2023-01-29 00:28:05 +0100

    formulaire d'ajout commentaire

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +14,23 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article/{id}", name="article")
      */
-    public function index(int $id, ArticleRepository $articleRepository): Response
+    public function index(int $id, ArticleRepository $articleRepository, Request $request): Response
     {
+        // Creating the form to add a comment under an article
+        $form = $this->createForm(CommentType::class);
+
+        // Handling information received with the form
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($form->getData());
+        }
+
         $article = $articleRepository->find($id);
+
         return $this->render('article/index.html.twig', [
             'article' => $article,
+            'form' => $form->createView(),
         ]);
     }
 }
