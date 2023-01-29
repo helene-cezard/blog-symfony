d8e3baa0c92fd53f475af2719a5fba3d1ec858d2
helene-cezard <helene.cezard@gmail.com>
2023-01-29 13:54:00 +0100

    Contraintes de validation User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +14,22 @@ class UserController extends AbstractController
     /**
      * @Route("/user/add", name="user_add")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $manager, Request $request): Response
     {
         // Creating the form to create a new user
         $form = $this->createForm(UserType::class);
+
+        // Handling information received with the form
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash('success', 'Vous êtes enregistré. Vous pouvez maintenant vous connecter.');
+        }
 
         return $this->renderForm('user/add.html.twig', [
             'form' => $form,
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,17 +4,37 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Username')
-            ->add('Email')
-            ->add('Password')
+            ->add('Username', null, [
+                'constraints' => new NotBlank,
+            ])
+            ->add('Email', EmailType::class, [
+                'constraints' => [
+                    new Email,
+                    new NotBlank,                    
+                ]
+            ])
+            ->add('Password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new NotBlank,
+
+                ]
+            ])
+            ->add('Submit', SubmitType::class)
         ;
     }
 
